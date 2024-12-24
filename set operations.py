7486a124fa282e787837d231ed4b59bbb2410c00
KAUSHIK1205 <chezhiworld@gmail.com>
2024-12-24 17:49:52 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,13 @@
+x={"india","russia","brazil","china","saudi arabia"}
+y={"austrlia","newzealand","india","south africa","west indies"}
+set1=x.intersection(y)
+print(set1)
+
+set2=x.union(y)
+print(set2)
+
+set3=x.difference(y)
+print(set3)
+
+set4=x.symmetric_difference(y)
+print(set4)
---
+++ sets and arrays.py
@@ -0,0 +1,16 @@
+set1={1,2,3,4,5,}
+print(set1)
+
+
+set2={"coding",1,2,3,4,5}
+print(set2)
+
+set3={1,2,3,4,4,5}
+print(set3)
+
+set4=set([1,2,3,4,5,6])
+print(set4)
+
+set5={2,3,4,5,6}
+set5.pop()
+print(set5)
